3cbaf753fda7ea834da0a644d71cfedc778cf352
Fabian Immel <fabian.immel@kit.edu>
2026-01-12 15:56:28 +0100

    add skeleton legacy untaggedDrivableAreaMode flag

--- lanelet2_ml_converter/include/lanelet2_ml_converter/MapData.h
+++ lanelet2_ml_converter/include/lanelet2_ml_converter/MapData.h
@@ -97,7 +97,8 @@ class LaneData {
 
   LaneData() noexcept : uuid_{boost::lexical_cast<std::string>(boost::uuids::random_generator()())} {}
   static LaneDataPtr build(LaneletSubmapConstPtr& localSubmap, lanelet::routing::RoutingGraphConstPtr localSubmapGraph,
-                           traffic_rules::TrafficRulesPtr trafficRules, bool ignoreMapElevation = false);
+                           traffic_rules::TrafficRulesPtr trafficRules, bool ignoreMapElevation = false,
+                           bool untaggedDrivableAreaMode = false);
   bool processAll(const OrientedRect& bbox, const ParametrizationType& paramType, int32_t nPoints, double pitch = 0,
                   double roll = 0);
 
@@ -143,7 +144,8 @@ class LaneData {
                             traffic_rules::TrafficRulesPtr trafficRules, bool ignoreMapElevation = false);
   void initCompoundInstances(LaneletSubmapConstPtr& localSubmap,
                              lanelet::routing::RoutingGraphConstPtr localSubmapGraph,
-                             traffic_rules::TrafficRulesPtr trafficRules, bool ignoreMapElevation = false);
+                             traffic_rules::TrafficRulesPtr trafficRules, bool ignoreMapElevation = false,
+                             bool untaggedDrivableAreaMode = false);
   void updateAssociatedCpdInstanceIndices();
   void getPaths(lanelet::routing::RoutingGraphConstPtr localSubmapGraph, std::vector<ConstLanelets>& paths,
                 ConstLanelet start, ConstLanelets initPath = ConstLanelets());
